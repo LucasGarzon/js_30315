309ceee0125e2f0647568f2fcd4d4678c732860c
Lucas Garzon <garzon.lucas.m@gmail.com>
2022-02-21 13:50:27 -0300

    Septimo commit: Cifrado americano incluido

--- javascript/complementarios/desafiocomp01.js
+++ javascript/complementarios/desafiocomp01.js
@@ -12,25 +12,25 @@ function botonA() {
         fundamental = 32.7
     } else if (nota == "do#" || nota == "reb" || nota == "c#" || nota == "db") {
         fundamental = 34.65
-    } else if (nota == "re") {
+    } else if (nota == "re" || nota == "d") {
         fundamental = 36.71
-    } else if (nota == "re#" || nota == "mib") {
+    } else if (nota == "re#" || nota == "mib" || nota == "d#" || nota == "eb") {
         fundamental = 38.89
-    } else if (nota == "mi" || nota == "fab") {
+    } else if (nota == "mi" || nota == "fab" || nota == "e" || nota == "fb") {
         fundamental = 20.6
-    } else if (nota == "fa" || nota == "mi#") {
+    } else if (nota == "fa" || nota == "mi#" || nota == "f" || nota == "e#") {
         fundamental = 21.83
-    } else if (nota == "fa#" || nota == "solb") {
+    } else if (nota == "fa#" || nota == "solb" || nota == "f#" || nota == "gb") {
         fundamental = 23.12
-    } else if (nota == "sol") {
+    } else if (nota == "sol" || nota == "g") {
         fundamental = 24.5
-    } else if (nota == "sol#" || nota == "lab") {
+    } else if (nota == "sol#" || nota == "lab" || nota == "g#" || nota == "ab") {
         fundamental = 25.96
-    } else if (nota == "la") {
+    } else if (nota == "la" || nota == "a") {
         fundamental = 27.5 
-    } else if (nota == "la#" || nota == "sib") {
+    } else if (nota == "la#" || nota == "sib" || nota == "a#" || nota == "bb") {
         fundamental = 29.14 
-    } else if (nota == "si" || nota == "dob") {
+    } else if (nota == "si" || nota == "dob" || nota == "b" || nota == "cb") {
         fundamental = 30.87
     } else {
         // Si el usuario ingresara otro valor a los anteriores, la variable fundamental se transformará en un string
