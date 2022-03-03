b45fbca5a7ee396e153b4ca85d1b790a7c3c1580
Lucas Garzon <garzon.lucas.m@gmail.com>
2022-03-02 21:25:50 -0300

    Decimoquinto commit: Desafío complementario 02 terminado

--- javascript/complementarios/desafiocomp02.js
+++ javascript/complementarios/desafiocomp02.js
@@ -1,33 +1,34 @@
 console.log("Calculadora de BPM a MS");
 
-function desafio01() {
-  //Toma el valor del input del HTML y lo comvierte a nuestros BPM de referencia
-
+function DesafioComplementario02() {
   let bpm = document.getElementById("miEntrada").value;
-  let figurasArray = [];
+  let resultadoConversor = [];
+  const figuras = [
+    "Redonda",
+    "Blanca",
+    "Negra",
+    "Corchea",
+    "Semicorchea",
+    "Fusa",
+    "Semifusa",
+  ];
   //Selecciona una figura rítmica
   let subdivision = parseInt(document.querySelector("#selector").value);
   //Este condicional sirve para limitar las posibilidades de ingreso de datos del usuario
   if (bpm > 0 && bpm <= 3000) {
-    // Se crea un array de referencia para cada valor que se obtenga
-    const figura = [
-      "Redonda",
-      "Blanca",
-      "Negra",
-      "Corchea",
-      "Semicorchea",
-      "Fusa",
-      "Semifusa",
-    ];
     // A continuación setea el mayor valor (figura musical redonda)
     let redonda = ((60000 / bpm) * 8).toFixed(1);
     // Con un loop for se obtienen las subdivisiones de Redonda
-    for (i = 0; i < figura.length; i++) {
+    for (i = 0; i < figuras.length; i++) {
       redonda = (redonda / subdivision).toFixed(1);
-      figurasArray.push(figura[i] + ": " + redonda + "ms " + "\n");
+      resultadoConversor.push(figuras[i] + ": " + redonda + "ms " + "\n");
     }
-    alert(figurasArray.join(""))
+    alert(resultadoConversor.join(""))
   } else {
     alert("Los BPM deben ser mayores a 0 y menores de 3000");
   }
 }
+
+
+
+
